23703f0957624a8d90657950725a392715f6a831
Douglas Zilli <douglaszilli@gmail.com>
2021-06-23 23:06:46 -0300

    Primeiro projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  // Para não precisar usar construtor ( Class Fields)
+  state = {
+    name: 'lorem ipsum',
+    counter: 0
+  };
+
+  handlePClick = () => {
+    this.setState({ name: 'lorem' })
+  }
+
+  // Hackzinho para não precisar criar um bind do método
+  handleAClick = (event) => {
+    // Evitamos que o site do react seja aberto. Cancela o evento de abrir a página.
+    event.preventDefault();
+
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 })
+  }
+
+  render() {
+
+    const { name, counter } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            {name} {counter}
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Este é o link
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 export default App;
