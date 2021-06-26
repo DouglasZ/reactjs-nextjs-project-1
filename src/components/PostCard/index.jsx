a1bc8bf5bbbe7edc8c95f006929aab20d2805deb
Douglas <douglaszilli@gmail.com>
2021-06-26 15:14:59 -0300

    Props e organização dos componentes - Parte 3

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,8 +1,10 @@
+import './styles.css';
+
 export const PostCard = ({ id, title, body, cover }) => (
   <div className="post">
     <img src={cover} alt={title} />
     <div className="post-content">
-      <h2>{title}</h2>
+      <h2>{id} - {title}</h2>
       <p>{body}</p>
     </div>
   </div>
